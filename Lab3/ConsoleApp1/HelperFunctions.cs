c674b902a2404fc8928a69fdd7d9bd9541865d46
mark-greenwood <markandrewgreenwood@gmail.com>
2021-10-31 18:45:13 -0700

    Lab 3 Completed

---
+++ Lab3/ConsoleApp1/HelperFunctions.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Linq;
+
+
+namespace Lab3Q1
+{
+    public class HelperFunctions
+    {
+        /**
+        * Counts number of words, separated by spaces, in a line.
+        * @param line string in which to count words
+        * @param start_idx starting index to search for words
+        * @return number of words in the line
+        */
+        public static int WordCount(ref string line, int start_idx)
+        {
+            // YOUR IMPLEMENTATION HERE
+            int count = 0;
+            bool characterIsSpace = false;
+            bool characterIsLetter = false;
+            bool previousCharacterIsSpace = false;
+            bool previousCharacterIsLetter = false;
+
+
+
+            for (int i = 0; i < (line.Length - start_idx); i++)
+            {
+                /* Console.WriteLine("Previous CharacterIsLetter Status {0}", previousCharacterIsLetter);
+                 Console.WriteLine("Previous CharacterIsSpace Status {0}", previousCharacterIsSpace);
+                 Console.WriteLine("CharacterIsLetter Status {0}", characterIsLetter);
+                 Console.WriteLine("CharacterIsSpace Status {0}", characterIsSpace);
+                 Console.WriteLine(line[i+start_idx]);
+                 Console.WriteLine("Count = {0}", count);
+                 Console.WriteLine();
+                */
+                previousCharacterIsSpace = characterIsSpace;
+                previousCharacterIsLetter = characterIsLetter;
+                characterIsSpace = false;
+                characterIsLetter = false;
+
+                if (char.IsWhiteSpace(line[i + start_idx]))
+                    characterIsSpace = true;
+                else
+                    characterIsLetter = true;
+
+                if (i == 0 && characterIsLetter == true)
+                {
+                    //  Console.WriteLine("First If True");
+                    count++;
+                }
+                else
+                {
+                    if ((previousCharacterIsSpace == true) && (characterIsLetter == true))
+                    {
+                        // Console.WriteLine("Second If True");
+                        count++;
+                    }
+                }
+            }
+
+
+            return count;
+
+
+        }
+
+
+
+        /**
+        * Reads a file to count the number of words each actor speaks.
+        *
+        * @param filename file to open
+        * @param mutex mutex for protected access to the shared wcounts map
+        * @param wcounts a shared map from character -> word count
+        */
+        public static void CountCharacterWords(string filename,
+                                 Mutex mutex,
+                                 Dictionary<string, int> wcounts)
+        {
+           
+            //===============================================
+            //  IMPLEMENT THIS METHOD INCLUDING THREAD SAFETY
+            //===============================================
+
+            string line;  // for storing each line read from the file
+            string speaker = "";  // empty character to start
+            System.IO.StreamReader file = new System.IO.StreamReader(filename);
+            int index = 0;
+            int numWords = 0;
+           
+        
+           
+            while ((line = file.ReadLine()) != null)
+            {
+                //=================================================
+                // YOUR JOB TO ADD WORD COUNT INFORMATION TO MAP
+                //=================================================
+
+                index = IsDialogueLine(line, ref speaker);
+                // Console.WriteLine(IsDialogueLine(line, ref character)); // Is the line a dialogueLine?
+                if (index > 0 && speaker != null)
+                {
+                    numWords = WordCount(ref line, index);
+                    mutex.WaitOne();
+                    if (wcounts.ContainsKey(speaker))
+                        wcounts[speaker] = wcounts[speaker] + numWords;
+                    else
+                        wcounts.Add(speaker, numWords);
+                    mutex.ReleaseMutex();
+                }
+               
+
+
+               
+
+            }
+
+            file.Close(); // Close the file
+
+
+        }
+
+
+
+        /**
+         * Checks if the line specifies a character's dialogue, returning
+         * the index of the start of the dialogue.  If the
+         * line specifies a new character is speaking, then extracts the
+         * character's name.
+         *
+         * Assumptions: (doesn't have to be perfect)
+         *     Line that starts with exactly two spaces has
+         *       CHARACTER. <dialogue>
+         *     Line that starts with exactly four spaces
+         *       continues the dialogue of previous character
+         *
+         * @param line line to check
+         * @param character extracted character name if new character,
+         *        otherwise leaves character unmodified
+         * @return index of start of dialogue if a dialogue line,
+         *      -1 if not a dialogue line
+         */
+        static int IsDialogueLine(string line, ref string character)
+        {
+
+            // new character
+            if (line.Length >= 3 && line[0] == ' '
+                && line[1] == ' ' && line[2] != ' ')
+            {
+                // extract character name
+
+                int start_idx = 2;
+                int end_idx = 3;
+                while (end_idx <= line.Length && line[end_idx - 1] != '.')
+                {
+                    ++end_idx;
+                }
+
+                // no name found
+                if (end_idx >= line.Length)
+                {
+                    return 0;
+                }
+
+                // extract character's name
+                character = line.Substring(start_idx, end_idx - start_idx - 1);
+                return end_idx;
+            }
+
+            // previous character
+            if (line.Length >= 5 && line[0] == ' '
+                && line[1] == ' ' && line[2] == ' '
+                && line[3] == ' ' && line[4] != ' ')
+            {
+                // continuation
+                return 4;
+            }
+
+            return 0;
+        }
+
+
+        /*
+         * Sorts characters in descending order by word count
+         *
+         * @param wcounts a map of character -> word count
+         * @return sorted vector of {character, word count} pairs
+         */
+        public static List<Tuple<int, string>> SortCharactersByWordcount(Dictionary<string, int> wordcount)
+        {
+
+            // Implement sorting by word count here
+            List<Tuple<int, string>> sortedByValueList = new List<Tuple<int, string>>();
+
+            foreach (KeyValuePair<string, int> character in wordcount.OrderByDescending(key => key.Value))
+            {
+                sortedByValueList.Add(new Tuple<int, string>(character.Value, character.Key));
+            }
+
+            return sortedByValueList;
+
+        }
+
+
+        /**
+         * Prints the List of Tuple<int, string>
+         *
+         * @param sortedList
+         * @return Nothing
+         */
+        public static void PrintListofTuples(List<Tuple<int, string>> sortedList)
+         {
+
+           foreach (Tuple<int, string> character in sortedList)
+            {
+                Console.WriteLine("Speaker: {0} has {1} words", character.Item2, character.Item1);
+            }    
+
+         }
+    }
+}
+
---
+++ Lab3/ConsoleApp1/WordCountTester.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Threading;
+using System.Collections.Generic;
+using System.IO;
+using System.Diagnostics;
+namespace Lab3Q1
+{
+    public class WordCountTester
+    {
+        static int Main()
+        {
+            Mutex mu = new Mutex();
+            Dictionary<string, int> wcountsST = new Dictionary<string, int>();
+            Dictionary<string, int> wcountsMT = new Dictionary<string, int>();
+            
+            DirectoryInfo d = new DirectoryInfo("..\\data\\"); //Set the directory to obtain files from
+
+            FileInfo[] Files = d.GetFiles("*.txt"); //Getting Text files
+            List<String> fpaths = new List<String>();
+            
+            foreach (FileInfo file in Files)
+            {
+                fpaths.Add("..\\data\\" + file.Name);
+            }
+
+            int numFiles = fpaths.Count;
+            List<Thread> threads = new List<Thread>();
+
+            Stopwatch stopWatchMT = new Stopwatch();
+            //TODO :start the stopwatch
+            stopWatchMT.Start();
+            for (int k =1; k < numFiles; k++)
+            {
+                int j = k;
+                Thread thread = new Thread(() => HelperFunctions.CountCharacterWords(fpaths[j],mu,wcountsMT));
+                thread.Name = string.Format("Thread{0}", j);
+                thread.Start();
+                threads.Add(thread);
+
+            }
+           
+            foreach (Thread thread in threads)
+                thread.Join();
+
+            //TODO :Stop the stopwatch
+            stopWatchMT.Stop();
+            // Get the elapsed time as a TimeSpan value.
+            TimeSpan tsMT = stopWatchMT.Elapsed;
+           
+
+            // Format and display the TimeSpan value.
+            string elapsedTimeMT = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+                tsMT.Hours, tsMT.Minutes, tsMT.Seconds,
+                tsMT.Milliseconds / 10);
+            Console.WriteLine("MT RunTime " + elapsedTimeMT);
+ 
+                
+            Stopwatch stopWatch = new Stopwatch();
+            //TODO :start the stopwatch
+            stopWatch.Start();
+
+            for (int k = 1; k < numFiles; k++)
+            {
+                HelperFunctions.CountCharacterWords(fpaths[k], mu, wcountsST);
+            }
+
+            //TODO :Stop the stopwatch
+            stopWatch.Stop();
+            // Get the elapsed time as a TimeSpan value.
+            TimeSpan ts = stopWatch.Elapsed;
+        
+            // Format and display the TimeSpan value.
+            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+                ts.Hours, ts.Minutes, ts.Seconds,
+                ts.Milliseconds / 10);
+            Console.WriteLine("ST RunTime " + elapsedTime);
+
+
+            List<Tuple<int, string>> MTWCount = HelperFunctions.SortCharactersByWordcount(wcountsMT);
+            List<Tuple<int, string>> STWCount = HelperFunctions.SortCharactersByWordcount(wcountsST);
+
+            Console.WriteLine("ST Results: ");
+            HelperFunctions.PrintListofTuples(STWCount);
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("MT Results: ");
+            HelperFunctions.PrintListofTuples(MTWCount);
+
+
+
+            string case1 = " 1 2 3 "; 
+            int case1Expected = 3;
+
+            string case2 = " 1, 2, 3, ";
+            int case2Expected = 3;
+
+            string case3 = " wasd, wasd: wasd; wasd-wasd, wasd+wasd";
+            int case3Expected = 5;
+
+            string case4 = "A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart. I am alone, and feel the charm of existence in this spot, which was created for the bliss of souls like mine. I am so happy ";
+            int case4Expected = 49;
+
+            string case5 = "A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart. I am alone, and feel the charm of existence in this spot, which was created for the bliss of souls like mine. I am so happy, my dear friend, so absorbed in the exquisite sense of mere tranquil existence, that I neglect my talents. I should be incapable of drawing a single stroke at the present moment; and yet I feel that I never was a greater artist than now. When, while the lovely";
+            int case5Expected = 97;
+
+            string case6 = "A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart. I am alone, and feel the charm of existence in this spot, which was created for the bliss of souls like mine. I am so happy, my dear friend, so absorbed in the exquisite sense of mere tranquil existence, that I neglect my talents. I should be incapable of drawing a single stroke at the present moment; and yet I feel that I never was a greater artist than now. When, while the lovely valley teems with vapour around me, and the meridian sun strikes the upper surface of the impenetrable foliage of my trees, and but a few stray gleams steal into the inner sanctuary, I throw myself down among the tall grass by the trickling stream; and, as I lie close to";
+            int case6Expected = 147;
+
+            string case7 = "A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart. I am alone, and feel the charm of existence in this spot, which was created for the bliss of souls like mine. I am so happy, my dear friend, so absorbed in the exquisite sense of mere tranquil existence, that I neglect my talents. I should be incapable of drawing a single stroke at the present moment; and yet I feel that I never was a greater artist than now. When, while the lovely valley teems with vapour around me, and the meridian sun strikes the upper surface of the impenetrable foliage of my trees, and but a few stray gleams steal into the inner sanctuary, I throw myself down among the tall grass by the trickling stream; and, as I lie close to the earth, a thousand unknown plants are noticed by me: when I hear the buzz of the little world among the stalks, and grow familiar with the countless indescribable forms of the insects and flies, then I feel the presence of the Almighty, who formed us in his own image";
+            int case7Expected = 197;
+
+            string case8 = "A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart. I am alone, and feel the charm of existence in this spot, which was created for the bliss of souls like mine. I am so happy, my dear friend, so absorbed in the exquisite sense of mere tranquil existence, that I neglect my talents. I should be incapable of drawing a single stroke at the present moment; and yet I feel that I never was a greater artist than now. When, while the lovely valley teems with vapour around me, and the meridian sun strikes the upper surface of the impenetrable foliage of my trees, and but a few stray gleams steal into the inner sanctuary, I throw myself down among the tall grass by the trickling stream; and, as I lie close to the earth, a thousand unknown plants are noticed by me: when I hear the buzz of the little world among the stalks, and grow familiar with the countless indescribable forms of the insects and flies, then I feel the presence of the Almighty, who formed us in his own image, and the breath of that universal love which bears and sustains us, as it floats around us in an eternity of bliss; and then, my friend, when darkness overspreads my eyes, and heaven and earth seem to dwell in my soul and absorb its power, like the form of a";
+            int case8Expected = 247;
+
+            string case9 = "A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart. I am alone, and feel the charm of existence in this spot, which was created for the bliss of souls like mine. I am so happy, my dear friend, so absorbed in the exquisite sense of mere tranquil existence, that I neglect my talents. I should be incapable of drawing a single stroke at the present moment; and yet I feel that I never was a greater artist than now. When, while the lovely valley teems with vapour around me, and the meridian sun strikes the upper surface of the impenetrable foliage of my trees, and but a few stray gleams steal into the inner sanctuary, I throw myself down among the tall grass by the trickling stream; and, as I lie close to the earth, a thousand unknown plants are noticed by me: when I hear the buzz of the little world among the stalks, and grow familiar with the countless indescribable forms of the insects and flies, then I feel the presence of the Almighty, who formed us in his own image, and the breath of that universal love which bears and sustains us, as it floats around us in an eternity of bliss; and then, my friend, when darkness overspreads my eyes, and heaven and earth seem to dwell in my soul and absorb its power, like the form of a beloved mistress, then I often think with longing, Oh, would I could describe these conceptions, could impress upon paper all that is living so full and warm within me, that it might be the mirror of my soul, as my soul is the mirror of the infinite God! O my friend";
+            int case9Expected = 298;
+
+            string case10 = "A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart. I am alone, and feel the charm of existence in this spot, which was created for the bliss of souls like mine. I am so happy, my dear friend, so absorbed in the exquisite sense of mere tranquil existence, that I neglect my talents. I should be incapable of drawing a single stroke at the present moment; and yet I feel that I never was a greater artist than now. When, while the lovely valley teems with vapour around me, and the meridian sun strikes the upper surface of the impenetrable foliage of my trees, and but a few stray gleams steal into the inner sanctuary, I throw myself down among the tall grass by the trickling stream; and, as I lie close to the earth, a thousand unknown plants are noticed by me: when I hear the buzz of the little world among the stalks, and grow familiar with the countless indescribable forms of the insects and flies, then I feel the presence of the Almighty, who formed us in his own image, and the breath of that universal love which bears and sustains us, as it floats around us in an eternity of bliss; and then, my friend, when darkness overspreads my eyes, and heaven and earth seem to dwell in my soul and absorb its power, like the form of a beloved mistress, then I often think with longing, Oh, would I could describe these conceptions, could impress upon paper all that is living so full and warm within me, that it might be the mirror of my soul, as my soul is the mirror of the infinite God! O my friend -- but it is too much for my strength -- I sink under the weight of the splendour of these visions! A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart. I am alone, and feel the charm of existence in this spot, which was created for the bliss of souls like mine. I am so happy, my dear friend, so absorbed in the exquisite sense of mere tranquil existence, that I neglect my talents. I should be incapable of drawing a single";
+            int case10Expected = 394;
+
+            string case11 = "A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart. I am alone, and feel the charm of existence in this spot, which was created for the bliss of souls like mine. I am so happy, my dear friend, so absorbed in the exquisite sense of mere tranquil existence, that I neglect my talents. I should be incapable of drawing a single stroke at the present moment; and yet I feel that I never was a greater artist than now. When, while the lovely valley teems with vapour around me, and the meridian sun strikes the upper surface of the impenetrable foliage of my trees, and but a few stray gleams steal into the inner sanctuary, I throw myself down among the tall grass by the trickling stream; and, as I lie close to the earth, a thousand unknown plants are noticed by me: when I hear the buzz of the little world among the stalks, and grow familiar with the countless indescribable forms of the insects and flies, then I feel the presence of the Almighty, who formed us in his own image, and the breath of that universal love which bears and sustains us, as it floats around us in an eternity of bliss; and then, my friend, when darkness overspreads my eyes, and heaven and earth seem to dwell in my soul and absorb its power, like the form of a beloved mistress, then I often think with longing, Oh, would I could describe these conceptions, could impress upon paper all that is living so full and warm within me, that it might be the mirror of my soul, as my soul is the mirror of the infinite God! O my friend -- but it is too much for my strength -- I sink under the weight of the splendour of these visions! A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart. I am alone, and feel the charm of existence in this spot, which was created for the bliss of souls like mine. I am so happy, my dear friend, so absorbed in the exquisite sense of mere tranquil existence, that I neglect my talents. I should be incapable of drawing a single stroke at the present moment; and yet I feel that I never was a greater artist than now. When, while the lovely valley teems with vapour around me, and the meridian sun strikes the upper surface of the impenetrable foliage of my trees, and but a few stray gleams steal into the inner sanctuary, I throw myself down among the tall grass by the trickling stream; and, as I lie close to the earth, a thousand unknown plants are noticed by me: when I hear the buzz of the little world among the stalks, and grow familiar with the";
+            int case11Expected = 493;
+
+
+            try {
+
+
+                //=================================================
+                // Implement your tests here. Check all the edge case scenarios.
+                // Create a large list which iterates over WCTester
+                //=================================================
+
+                WCTester(case1, 0, case1Expected);
+                WCTester(case2, 0, case2Expected);
+                WCTester(case3, 0, case3Expected);
+                WCTester(case4, 0, case4Expected);
+                WCTester(case5, 0, case5Expected);
+                WCTester(case6, 0, case6Expected);
+                WCTester(case7, 0, case7Expected);
+                WCTester(case8, 0, case8Expected);
+                WCTester(case9, 0, case9Expected);
+                WCTester(case10, 0, case10Expected);
+                WCTester(case11, 0, case11Expected);
+
+
+
+                //WCTester(line, startIdx, expectedResults);
+
+            } catch(UnitTestException e) {
+              Console.WriteLine(e);
+            }
+            
+            return 0;
+        }
+
+
+        /**
+         * Tests word_count for the given line and starting index
+         * @param line line in which to search for words
+         * @param start_idx starting index in line to search for words
+         * @param expected expected answer
+         * @throws UnitTestException if the test fails
+         */
+          static void WCTester(string line, int start_idx, int expected) {
+
+            //=================================================
+            // Implement: comparison between the expected and
+            // the actual word counter results
+            //=================================================
+            int result = HelperFunctions.WordCount(ref line, start_idx);
+            if (result != expected) {
+              throw new Lab3Q1.UnitTestException(ref line, start_idx, result, expected, String.Format("UnitTestFailed: result:{0} expected:{1}, line: {2} starting from index {3}", result, expected, line, start_idx));
+            }
+
+           }
+    }
+}
